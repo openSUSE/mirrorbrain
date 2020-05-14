ab20375c8b2122a0bbae512adcb632eba7479305
Andrii Nikitin <anikitin@suse.de>
2020-05-14 19:20:36 +0200

    Add environs ssl+rsync test

--- mb/mb/testmirror.py
+++ mb/mb/testmirror.py
@@ -40,11 +40,16 @@ def probe(S, http_method='GET'):
 
     if S.scheme in ['http', 'https', 'ftp']:
         req = urllib.request.Request(S.probeurl)
-        if S.scheme == 'http' and http_method == 'HEAD':
+        if S.scheme.startswith('http') and http_method == 'HEAD':
             # not for FTP URLs
             req.get_method = lambda: 'HEAD'
 
         try:
+            try:
+                import ssl
+                ssl._create_default_https_context = ssl._create_unverified_context
+            except Exception:
+                pass
             response = urllib.request.urlopen(req)
         except KeyboardInterrupt:
             print('interrupted!', file=sys.stderr)
@@ -70,7 +75,7 @@ def probe(S, http_method='GET'):
         if S.get_content:
             S.content = response.read()
 
-        if S.scheme == 'http':
+        if S.scheme.startswith('http'):
             S.http_code = getattr(response, "code", None)
             if S.http_code == 200:
                 S.has_file = True
--- t/docker/environs/03-ssl.sh
+++ t/docker/environs/03-ssl.sh
@@ -1,5 +1,5 @@
 #!../lib/test-in-container-environs.sh
-set -ex
+set -e
 
 [ -d mirrorbrain ]
 
@@ -8,6 +8,7 @@ set -ex
 ./environ.sh ap8-system2
 ./environ.sh ap7-system2
 ./environ.sh mb9 $(pwd)/mirrorbrain
+./environ.sh rs9-system2
 
 pg9*/start.sh
 
@@ -21,11 +22,14 @@ for x in ap7 ap8 ap9; do
     xx=$(ls -d $x*/)
     mkdir -p $xx/dt/downloads/{folder1,folder2,folder3}
     echo $xx/dt/downloads/{folder1,folder2,folder3}/{file1,file2}.dat | xargs -n 1 touch
+    rs9*/configure_dir.sh $x $(pwd)/$xx/dt
 done
 
 mkdir -p ap9-system2/hashes
 
-mb9*/mb.sh makehashes $PWD/ap9-system2/dt/downloads -t $PWD/ap9-system2/hashes/downloads
+mb9*/mb.sh makehashes $PWD/ap9-system2/dt -t $PWD/ap9-system2/hashes
+rs9*/start.sh
+rs9*/status.sh
 
 ap9*/start.sh
 ap9*/status.sh
@@ -33,9 +37,10 @@ ap9*/status.sh
 ap9*/curl.sh downloads/ | grep folder1
 
 for x in ap7 ap8; do
+    $x*/configure_ssl.sh
     $x*/start.sh
     $x*/status.sh
-    mb9*/mb.sh new $x --http http://"$($x-system2/print_address.sh)" --region NA --country us
+    mb9*/mb.sh new $x --http https://"$($x-system2/print_address.sh)" --rsync rsync://$(id -un):$(id -un)@127.0.0.1:9090/$x  --region NA --country us
     mb9*/mb.sh scan --enable $x
     $x-system2/curl.sh | grep downloads
 done
