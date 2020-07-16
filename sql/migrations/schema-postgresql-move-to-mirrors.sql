e9ee071e57435c7e79033999cbe72e02456ef521
Marcus Rückert <darix@nordisch.org>
2020-07-16 11:37:08 +0200

    Port the hexhash view and helper functions

--- sql/migrations/schema-postgresql-move-to-mirrors.sql
+++ sql/migrations/schema-postgresql-move-to-mirrors.sql
@@ -96,3 +96,204 @@ AS $BODY$
         WHERE count = 0
   ) RETURNING *;
 END $BODY$;
+
+-- FUNCTION: public.mirr_add_byid(integer, integer)
+
+DROP FUNCTION IF EXISTS public.mirr_add_byid(integer, integer);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_add_file(
+    arg_serverid integer,
+    arg_fileid integer
+  )
+    RETURNS integer
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+BEGIN
+  INSERT INTO filemetadata (filemetadata_id, arg_serverid)
+    VALUES (arg_fileid, arg_serverid)
+    ON CONFLICT
+    DO NOTHING RETURNING *;
+END;
+$BODY$;
+
+-- FUNCTION: public.mb_add_bypath(integer, text)
+
+DROP FUNCTION IF EXISTS public.mirr_add_bypath(integer, text);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_add_file(
+    arg_serverid integer,
+    arg_path text
+  )
+    RETURNS integer
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    fileid integer;
+BEGIN
+    SELECT INTO fileid id FROM filemetadata WHERE path = arg_path;
+
+    -- There are three cases to handle, and we want to handle each of them
+    -- with the minimal effort.
+    -- In any case, we return a file id in the end.
+    IF fileid IS NULL THEN
+        RAISE DEBUG 'we do not know about the file "%".', arg_path;
+    ELSE
+        RAISE DEBUG 'update existing file entry (path: % id: %)', arg_path, fileid;
+        SELECT into fileid mirr_add_byid(arg_serverid, fileid);
+    END IF;
+
+    RETURN fileid;
+END;
+$BODY$;
+
+-- FUNCTION: public.mirr_del_byid(integer, integer)
+
+DROP FUNCTION IF EXISTS public.mirr_del_byid(integer, integer);
+
+--
+-- TODO: for consistency with the mb_mirror_add_file() function se should probably also add
+--       mb_mirror_remove_file(integer, text)
+CREATE OR REPLACE FUNCTION public.mb_mirror_remove_file(
+    arg_serverid integer,
+    arg_fileid integer
+  )
+    RETURNS integer
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+BEGIN
+  DELETE FROM mirrors WHERE server_id = arg_serverid AND filemetadata_id = arg_fileid RETURNING *;
+END;
+$BODY$;
+
+
+-- FUNCTION: public.mirr_get_name(integer)
+
+DROP FUNCTION IF EXISTS public.mirr_get_name(integer);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_identifier(
+    arg_serverid integer
+  )
+    RETURNS text
+    LANGUAGE 'sql'
+AS $BODY$
+  SELECT identifier FROM server WHERE id=arg_serverid
+$BODY$;
+
+-- FUNCTION: public.mirr_get_name(smallint[])
+
+DROP FUNCTION IF EXISTS public.mirr_get_name(smallint[]);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_identifier(
+    ids smallint[]
+  )
+    RETURNS text[]
+    LANGUAGE 'sql'
+AS $BODY$
+  SELECT array_agg(identifier::text) FROM server WHERE id = ANY(ids);
+$BODY$;
+
+-- FUNCTION: public.mirr_get_nfiles(integer)
+
+DROP FUNCTION IF EXISTS public.mirr_get_nfiles(integer);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_filecount(
+	  arg_serverid integer
+  )
+    RETURNS bigint
+    LANGUAGE 'sql'
+AS $BODY$
+  SELECT count(*) FROM mirrors WHERE server_id = arg_serverid;
+$BODY$;
+
+-- FUNCTION: public.mirr_get_nfiles(text)
+
+DROP FUNCTION IF EXISTS public.mirr_get_nfiles(text);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_filecount(
+	  arg_server_identifier text
+  )
+    RETURNS bigint
+    LANGUAGE 'sql'
+AS $BODY$
+  SELECT count(*) FROM mirrors WHERE (SELECT id FROM server WHERE identifier = arg_server_identifier) = server_id;
+$BODY$;
+
+-- FUNCTION: public.mirr_hasfile_byid(integer, integer)
+
+DROP FUNCTION IF EXISTS public.mirr_hasfile_byid(integer, integer);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_hasfile(
+    arg_serverid integer,
+    arg_fileid integer
+  )
+    RETURNS boolean
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    result integer;
+BEGIN
+    SELECT INTO result 1 FROM mirrors WHERE filemetadata_id = arg_fileid AND server_id = arg_serverid;
+    IF result > 0 THEN
+        RETURN true;
+    END IF;
+    RETURN false;
+END;
+$BODY$;
+
+-- FUNCTION: public.mirr_hasfile_byname(integer, text)
+
+DROP FUNCTION IF EXISTS public.mirr_hasfile_byname(integer, text);
+
+CREATE OR REPLACE FUNCTION public.mb_mirror_hasfile(
+    arg_serverid integer,
+    arg_path text
+  )
+    RETURNS boolean
+    LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    result integer;
+    fileid integer;
+BEGIN
+    SELECT INTO fileid id FROM filemetadata WHERE path = arg_path;
+
+    -- There are three cases to handle, and we want to handle each of them
+    -- with the minimal effort.
+    -- In any case, we return a file id in the end.
+    IF fileid IS NULL THEN
+        RAISE DEBUG 'we do not know about the file "%".', arg_path;
+    ELSE
+      SELECT INTO result 1 FROM mirrors WHERE filemetadata_id = fileid AND server_id = arg_serverid;
+      IF result > 0 THEN
+          RETURN true;
+      END IF;
+    END IF;
+    RETURN false;
+END;
+$BODY$;
+
+DROP VIEW IF EXISTS hexhash;
+CREATE VIEW hexhash AS
+  SELECT
+    id AS file_id,
+    mtime,
+    size,
+    md5,
+    encode(md5, 'hex') AS md5hex,
+    sha1,
+    encode(sha1, 'hex') AS sha1hex,
+    sha256,
+    encode(sha256, 'hex') AS sha256hex,
+    sha1piecesize,
+    sha1pieces,
+    encode(sha1pieces, 'hex') AS sha1pieceshex,
+    btih,
+    encode(btih, 'hex') AS btihhex,
+    pgp,
+    zblocksize,
+    zhashlens,
+    zsums,
+    encode(zsums, 'hex') AS zsumshex
+  FROM filemetadata;
